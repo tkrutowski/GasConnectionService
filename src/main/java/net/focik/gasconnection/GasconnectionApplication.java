a5bd16855c7aff33bff53ad4e6d7a64712b79bae
Tomasz Krutowski <tkrutowski@gmail.com>
2021-02-19 09:26:18 +0100

    init

---
+++ src/main/java/net/focik/gasconnection/GasconnectionApplication.java
@@ -0,0 +1,13 @@
+package net.focik.gasconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasconnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasconnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/focik/gasconnection/GasconnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package net.focik.gasconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasconnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
